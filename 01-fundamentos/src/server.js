c3989d96c232a3195e7c2be660d0430e1f7a20f1
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2025-12-08 13:20:14 -0300

    feat(database): implement in-memory database with file persistence and user management

--- 01-fundamentos/src/server.js
+++ 01-fundamentos/src/server.js
@@ -1,31 +1,31 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { json } from './middlewares/json.js'
+import { Database } from './database.js'
 
-const users = []
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
-
   await json(req, res)
 
   if (method === 'GET' && url === '/users') {
-    return res
-
-      .end(JSON.stringify(users))
+    const users = database.select('users')
+    return res.end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body
-
-    users.push({
-      id: 1,
+    const user ={
+      id: randomUUID(),
       name,
       email,
-    })
-
-    return res.writeHead(201).end()
+    }
+    database.insert('users', user)
+    return res.writeHead(201).end(
+      "User created successfully!"
+    )
   }
-
   return res.writeHead(404).end()
 })
 
