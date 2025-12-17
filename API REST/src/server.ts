add5540725e51a4f43f1f57859ccfe04069074eb
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2025-12-17 20:50:04 -0300

    feat: integrate knex for database management and create my first migration

--- API REST/src/server.ts
+++ API REST/src/server.ts
@@ -1,11 +1,15 @@
 import fastify from 'fastify';
+import { knex } from './database.js';
+
 
 const app = fastify();
 
 // Define a rota
 // localhost:3000/
-app.get('/', () => {
-  return 'Hello World';
+app.get('/', async () => {
+  const database = await knex('sqlite_schema').select('*')
+  return database
+
 })
 
 app.listen({ port: 3000 }).then(() => {
