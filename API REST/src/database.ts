93a3e7d6f39a8ccf360ad41930b762225b4f2d9f
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-01-06 22:36:21 -0300

    chore: production config

--- API REST/src/database.ts
+++ API REST/src/database.ts
@@ -3,16 +3,17 @@ import 'dotenv/config'
 import knex from 'knex'
 import type { Knex } from 'knex'
 import { env } from './env/index.js'
+import { en } from 'zod/locales'
 
 if (!env.DATABASE_URL) {
   throw new Error('DATABASE ENVIRONMENT NOT SET')
 }
 
 export const config: Knex.Config = {
-  client: 'sqlite3',
-  connection: {
-    filename: env.DATABASE_URL
-  },
+  client: env.DATABASE_CLIENT,
+  connection: env.DATABASE_URL === 'sqlite'
+    ? { filename: env.DATABASE_URL }
+    : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
--- API REST/src/env/index.ts
+++ API REST/src/env/index.ts
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
 })
 
 const _env = envSchema.safeParse(process.env)
