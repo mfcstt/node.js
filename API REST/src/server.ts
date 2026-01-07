59c1818f26cd4bcdbab069e37e846f4b51471b5f
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-01-06 22:54:43 -0300

    chore: production config

--- API REST/src/server.ts
+++ API REST/src/server.ts
@@ -2,6 +2,6 @@ import { app } from './app.js';
 import { env } from './env/index.js';
 
 
-app.listen({ port: env.PORT }).then(() => {
+app.listen({ port: env.PORT, host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost' }).then(() => {
     console.log(`Server running on http://localhost:${env.PORT}`);
   });
