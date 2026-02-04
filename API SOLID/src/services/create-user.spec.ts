7e204bf240658deef3b75440cf4af730b9670585
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-02-04 12:55:40 -0300

    test: add email verification and user creation tests with coverage

--- API SOLID/src/services/create-user.spec.ts
+++ API SOLID/src/services/create-user.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, test } from 'vitest'
 import { UserService } from './create-user-service'
 import { compare } from 'bcryptjs'
 import {InMemoryUsersRepository}  from '@/repositories/in-memory/in-memory-users-repository'
+import { UserAlreadyExistsError } from './erros/user-alredy-exists-error'
 
 // unit tests for create-user service
 describe('Register User Service', () => {
@@ -22,5 +23,38 @@ describe('Register User Service', () => {
       user.password_hash,
     )
     expect(isPassordCorrectlyHashed).toBe(true)
+  })
+
+  it('should not allow registration with an existing email', async () => {
+    const usersRepositoryMock = new InMemoryUsersRepository()
+    const userService = new UserService(usersRepositoryMock)
+
+    const email = 'john.doe@example.com'
+
+    await userService.register({
+      name: 'John Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(async () => {
+      await userService.register({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      })
+    }).rejects.toBeInstanceOf(UserAlreadyExistsError)
+  })
+
+  it('should register a user successfully', async () => {
+    const usersRepositoryMock = new InMemoryUsersRepository()
+    const userService = new UserService(usersRepositoryMock)  
+
+    const { user } = await userService.register({
+      name: 'Jane Doe',
+      email: 'jane.doe@example.com',
+      password: '123456',
+    })
+    expect(user.id).toEqual(expect.any(String))
   })
 })
