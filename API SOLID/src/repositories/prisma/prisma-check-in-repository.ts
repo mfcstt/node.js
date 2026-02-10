1e91afeeff8dbc675fca1cd0e0d0dc049c1ef48a
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-02-10 15:35:38 -0300

    feat: implement Prisma repositories based on defined interfaces

--- API SOLID/src/repositories/prisma/prisma-check-in-repository.ts
+++ API SOLID/src/repositories/prisma/prisma-check-in-repository.ts
@@ -1,15 +1,44 @@
 import { prisma } from "@/lib/prisma";
-import type { Prisma } from "prisma/generated";
+import type { CheckIn, Prisma } from "prisma/generated";
 import type { CheckInRepository } from "../check-in-repository";
 
 export class PrismaCheckInRepository implements CheckInRepository {
-  async create(data: Prisma.CheckInUncheckedCreateInput) {
-    const checkIn = await prisma.checkIn.create({
-      data,
-    })
+  async findById(id: string) {
+    const checkIn = await prisma.checkIn.findUnique({
+      where: {
+        id,
+      },
+    });
     return checkIn
   }
 
+  async findManyByUserId(userId: string, page: number) {
+    const checkIns = await prisma.checkIn.findMany({
+      where: {
+        user_id: userId
+      },
+      take: 20,
+      skip: (page-1) * 20,
+    })
+    return checkIns
+  }
+  async countByUserId(userId: string) {
+    const count = await prisma.checkIn.count({
+      where: {
+        user_id: userId
+      }
+    })
+    return count
+  }
+  async save(data: CheckIn){
+    const checkIn = await prisma.checkIn.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    });
+    return checkIn
+  }
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfDay = new Date(date)
     startOfDay.setHours(0, 0, 0, 0)
@@ -26,6 +55,13 @@ export class PrismaCheckInRepository implements CheckInRepository {
       },
     })
 
+    return checkIn
+  }
+
+   async create(data: Prisma.CheckInUncheckedCreateInput) {
+    const checkIn = await prisma.checkIn.create({
+      data,
+    })
     return checkIn
   }
 }
