f228f9dc9dfa0a51c8d29362d5ff4d5629b94466
Maria Fernanda Rizzo <126708422+mfcstt@users.noreply.github.com>
2026-02-10 15:50:57 -0300

    feat: implement Gym repository with Prisma based on contract interface

---
+++ API SOLID/src/repositories/prisma/prisma-gyms-repository.ts
@@ -0,0 +1,41 @@
+import type { Gym, Prisma } from "prisma/generated";
+import type { FindManyNearbyGymsParams, GymsRepository } from "../gyms-repository";
+import { prisma } from "@/lib/prisma";
+
+
+export class PrismaGymsRepository implements GymsRepository{
+
+  async findById(id: string) {
+    const gym = await prisma.gym.findUnique({
+      where: {
+        id,
+      }
+    });
+    return gym
+  }
+  async create(data: Prisma.GymCreateInput) {
+    const gym = await prisma.gym.create({
+      data,
+    });
+    return gym
+  }
+  async searchMany(query: string, page: number) {
+    const gyms = await prisma.gym.findMany({
+      where: {
+        name: {
+          contains: query
+        },
+      },
+      take: 20,
+      skip: (page-1) * 20
+    })
+    return gyms
+  }
+  async findManyNearby({userLatitude, userLongitude}: FindManyNearbyGymsParams) {
+    const gyms = await prisma.$queryRaw<Gym[]>`
+      SELECT * FROM gyms
+      WHERE ( 6371 * acos( cos( radians(${userLatitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${userLongitude}) ) + sin( radians(${userLatitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+  return gyms
+  }
+}
