0a488e0df584cf470cb02eb3898998b765ca41d6
mfcstt <mahcosta54@gmail.com>
2026-03-16 20:27:45 -0300

    feat: implement Slug value object for Question entity

---
+++ DDD/src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,9 @@
+import {expect, test} from "vitest";
+import { Slug } from "./slug.js";
+
+test("it should be able to create a slug from a string", () => {
+  const slug = Slug.createFromText("How to create a slug?");
+
+  expect(slug.text).toBe("how-to-create-a-slug");
+  
+});
---
+++ DDD/src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,26 @@
+export class Slug{
+  public text: string;
+
+  constructor(text: string) {
+    this.text = text;
+  }
+
+  /**
+   * Receives a string and normalize it as a slug
+   * Example: "How to create a slug?" => "how-to-create-a-slug"
+   * @param text {string}
+   */
+  static createFromText(text: string){
+    const slugText = text
+    .normalize("NFKD")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, '-')
+    .replace(/[^\w-]+/g, '')
+    .replace(/_/g, '-')
+    .replace(/--/g, '-')
+    .replace(/-$/g, '')
+
+    return new Slug(slugText);  
+  }
+}
