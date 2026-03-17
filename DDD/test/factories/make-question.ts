6555be8528c3d0dda7e5dcf11109d7ba456bc34f
mfcstt <mahcosta54@gmail.com>
2026-03-17 19:27:46 -0300

    test: add faker for generating mock data

--- DDD/test/factories/make-question.ts
+++ DDD/test/factories/make-question.ts
@@ -1,13 +1,14 @@
 import { Question, type QuestionProps } from "@/domain/forum/enterprise/entities/question.js";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug.js";
 import { UniqueEntityID } from "@/domain/forum/enterprise/entities/value-objects/unique-entity-id.js";
+import {faker} from '@faker-js/faker'
 
 export function makeQuestion(override: Partial<QuestionProps> = {}) {
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: 'Example question',
-    slug: Slug.create('example-question'),
-    content: 'Example content',
+    title: faker.lorem.sentence(),
+    slug: Slug.create(faker.lorem.slug()),
+    content: faker.lorem.paragraph(),
     ...override,
   })
 
