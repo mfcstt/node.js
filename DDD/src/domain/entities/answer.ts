d6bc0ee4af3b28eb12d1f1f1ba194d30dd425e21
mfcstt <mahcosta54@gmail.com>
2026-03-16 21:38:18 -0300

    feat: map properties for Answer and Question entities

--- DDD/src/domain/entities/answer.ts
+++ DDD/src/domain/entities/answer.ts
@@ -1,9 +1,12 @@
 import { Entity } from "../../core/entities/entity.js";
+import type { UniqueEntityID } from "./value-objects/unique-entity-id.js";
 
 interface AnswerProps {
   content: string;
-  authorId: string;
-  questionId: string;
+  authorId: UniqueEntityID;
+  questionId: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Answer extends Entity<AnswerProps> {
--- DDD/src/domain/entities/question.ts
+++ DDD/src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import type { Slug } from "./value-objects/slug.js";
 import { Entity } from "../../core/entities/entity.js";
+import type { UniqueEntityID } from "./value-objects/unique-entity-id.js";
 
 interface QuestionProps {
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   title: string;
   slug: Slug;
   content: string;
-  authorId: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {
