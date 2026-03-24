4a8f5fdb65a3d2c2258c73491a8b4c33a90e13ce
mfcstt <mahcosta54@gmail.com>
2026-03-24 19:17:35 -0300

    feat: implement CommentOnAnswer use case and in-memory repository for answer comments

---
+++ DDD/src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -0,0 +1,5 @@
+import type { AnswerComment } from "../../enterprise/entities/answer-comment.js";
+
+export interface AnswerCommentsRepository {
+  create(answerComment: AnswerComment): Promise<void>
+}
