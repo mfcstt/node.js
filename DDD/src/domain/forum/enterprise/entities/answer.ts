ae5edd969da674c3f2a56bc7c07d1e4ca32ce308
mfcstt <mahcosta54@gmail.com>
2026-04-13 21:30:43 -0300

    feat: refactor Answer entity to extend AggregateRoot and add AnswerCreatedEvent

--- DDD/src/domain/forum/enterprise/entities/answer.ts
+++ DDD/src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity.js'
 import type { Optional } from '@/core/types/optional.js'
 import type { UniqueEntityID } from '../../../../core/entities/unique-entity-id.js'
 import { AnswerAttachmentList } from './answer-attachment-list.js'
+import { AggregateRoot } from '@/core/entities/aggregate-root.js'
+import { AnswerCreatedEvent } from '../events/answer-created-event.js'
 
 export interface AnswerProps {
   content: string
@@ -12,7 +13,7 @@ export interface AnswerProps {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get authorId() {
     return this.props.authorId
   }
@@ -68,6 +69,13 @@ export class Answer extends Entity<AnswerProps> {
       },
       id,
     )
+
+    const isNewAnswer = !id
+
+    if(isNewAnswer){
+      answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    }
+
     return answer
   }
 }
---
+++ DDD/src/domain/forum/enterprise/events/answer-created-event.ts
@@ -0,0 +1,16 @@
+import type { UniqueEntityID } from "@/core/entities/unique-entity-id.js";
+import type { Answer } from "../entities/answer.js";
+
+export class AnswerCreatedEvent {
+  public ocurredAt: Date
+  public answer: Answer
+
+  constructor(answer: Answer){
+    this.answer = answer
+    this.ocurredAt = new Date()
+  }
+
+  getAggregateId(): UniqueEntityID{
+    return this.answer.id
+  }
+}
